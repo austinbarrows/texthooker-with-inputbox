b5572ed7560ea8c6f54d318f4cdda0d7bb197aea
austinbarrows <austinm.barrows@gmail.com>
2022-01-30 22:24:32 -0500

    Add addLine function to add lines on form submit

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+function addLine() {
+    const lineData = document.getElementById("line-input").value;
+    const p = document.createElement("p");
+    p.append(lineData);
+    const pasteBox = document.getElementById('autopaste-box');
+    pasteBox.append(p);
+}
+
+
 const el = document.getElementById('autopaste-box');
-
-
 const callback = function(mutationsList, observer) {
     console.log('working');
     window.scrollTo(0, document.body.scrollHeight);
