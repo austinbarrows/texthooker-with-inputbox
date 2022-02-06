a79c93d6e06ba67c1b0982017f09a4a2b17efc8e
austinbarrows <austinm.barrows@gmail.com>
2022-02-06 00:58:17 -0500

    Change scroll adjusting code to match bugfix code

--- main.js
+++ main.js
@@ -19,7 +19,8 @@ window.onfocus = function () {
 // Code to scroll down every time body is modified (i.e. when <p> elements are added)
 const el = document.getElementById('autopaste-box');
 const callback = function(mutationsList, observer) {
-    window.scrollTo(0, document.body.scrollHeight);
+    const pasteBox = document.getElementById('autopaste-box');
+    pasteBox.scrollTo(0, pasteBox.scrollHeight);
 };
 const config = { attributes: true, childList: true, subtree: true };
 const observer = new MutationObserver(callback);
